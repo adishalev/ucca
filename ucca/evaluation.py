4257830afba19a1ed87fd14957bb60e46ffc80c5
adi <adi.b.shalev@gmail.com>
2019-04-11 11:18:06 +0300

    -delete print for debug

--- ucca/evaluation.py
+++ ucca/evaluation.py
@@ -235,8 +235,6 @@ class Scores:
                 for x in constructions for y in titles]
 
     def __getitem__(self, eval_type):
-        if eval_type not in self.evaluators:
-            print("here")
         return self.evaluators[eval_type]
 
 
